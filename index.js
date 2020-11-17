2cfd3707082e201776d0c932d04233d0cd6a7a20
jake-utk <jakeryanaddis@gmail.com>
2020-11-17 16:19:38 -0600

    Added heroList controller and model

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var Key = config.SECRET_KEY;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const heroListsController = require("./controllers/hero-lists");
+app.use("/api/herolist", heroListsController);
+app.get("/", (req, res) => {
+	res.redirect("/api/herolist");
+});
+app.set("port", process.env.PORT || 8000);
+
+app.listen(app.get("port"), () => {
+	console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
