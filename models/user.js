ad4e03e65d5ff26b0e6608977b3a6232b2341eeb
nneverson <neelyneverson@yahoo.com>
2020-11-18 10:39:46 -0500

    create user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('../db/connection');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+    {
+        name: String,
+        email: {type: String,
+        unique: true},
+        password: String,
+    });
+
+    const User = mongoose.model ('User', PersonSchema)
+
+    module.exports = User
