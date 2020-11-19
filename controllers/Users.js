4c78fbfd56087ae87cce0a1500ce278020d53e8f
nneverson <neelyneverson@yahoo.com>
2020-11-19 09:50:03 -0500

    add catch.next in users controller

--- controllers/Users.js
+++ controllers/Users.js
@@ -5,37 +5,42 @@ const router = express.Router();
 
 //Get all users
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
 	Users.find({}).then((user) => {
 		res.json(user);
-	});
+	})
+	.catch(next)
 });
 
 //Find one user
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
 	Users.findById({ _id: req.params.id }).then((user) => {
 		res.json(user);
-	});
+	})
+	.catch(next)
 });
 
 //Post (create a user)
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
 	Users.create(req.body).then((user) => {
 		res.json(user);
-	});
+	})
+	.catch(next)
 });
 //Patch ( edit a user)
-router.patch('/:id', (req, res) => {
+router.patch('/:id', (req, res, next) => {
 	User.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
 		(user) => {
 			res.json(user);
 		}
-	);
+	)
+	.catch(next)
 });
 // Delete a user
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     User.findByIdAndDelete({_id: req.params.id}).then((user) => {
         res.json(user)
-    })
+	})
+	.catch(next)
 })
 module.exports = router;
