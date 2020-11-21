1b4b123c96dbeee87c94c77ff78f0421e50eba42
rodrigojpt <rodrigojpereirat@gmail.com>
2020-11-20 23:15:31 -0500

    updated list schema in user model reference

--- db/seeds.js
+++ db/seeds.js
@@ -1,26 +1,27 @@
-const List = require('../models/list')
-const User = require('../models/user')
+const List = require('../models/list');
+const User = require('../models/user');
 
-const listSeedData = require('./list.json'); 
+const listSeedData = require('./list.json');
 
-const userSeedData = require('./user.json'); 
-
-List.deleteMany({}).then(() => {
-    return List.insertMany(listSeedData);
-})
-.then(console.log)
-.catch(console.error)
-.finally(() => {
-    process.exit();
-}); 
+const userSeedData = require('./user.json');
 
 User.deleteMany({})
-.then(() => {
-    return User.insertMany(userSeedData);
-})
-.then(console.log)
-.catch(console.error)
-.finally(() => {
-    process.exit();
-});
-
+	.then(() => User.create(userSeedData))
+	.then((user) => {
+		List.deleteMany({})
+			.then(() => {
+				return List.create([
+					{
+						...listSeedData,
+						owner: user._id,
+					},
+				]);
+			})
+			.then(console.log)
+			.catch(console.error)
+			.finally(() => {
+				process.exit();
+			});
+	})
+	.then(console.log)
+	.catch(console.error);
